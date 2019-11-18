dceee91ad0bc676fb0885f2e6f2a0323c5f1499c
rsajko <50842045+rsajko@users.noreply.github.com>
2019-11-19 02:45:37 +0300

    fixed platforms + control

--- Extra-Jump/Assets/Scripts/Mike.cs
+++ Extra-Jump/Assets/Scripts/Mike.cs
@@ -2,32 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mike : MonoBehaviour
-{
-    float horizontal;
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Mike : MonoBehaviour
+    {
+    public float movementSpeed = 10f;
     Rigidbody2D rb;
+    float horizontal;
 
     void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
 
-    void Update()
+    void FixedUpdate()
     {
-        if(Application.platform==RuntimePlatform.Android)
-        { horizontal = Input.acceleration.x;
-        }else
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            horizontal = Input.acceleration.x;
+        }
+        else
         {
             horizontal = Input.GetAxis("Horizontal");
         }
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * 10f, rb.velocity.y);
     }
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.gameObject.tag=="Platform")
+
+    void Update()
         {
-            rb.velocity = Vector2.zero;
-            rb.AddForce(transform.up * 20, ForceMode2D.Impulse);
+            Vector2 velocity = rb.velocity;
+            velocity.x = 0f;
+            rb.velocity = velocity;
         }
     }
-}
+
