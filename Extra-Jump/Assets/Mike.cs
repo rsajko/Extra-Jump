8e5978b7808aaa9fc07e500ec18e5a8aacc28e7b
rsajko <50842045+rsajko@users.noreply.github.com>
2019-11-19 00:29:57 +0300

    PNG + "Jump"Script

---
+++ Extra-Jump/Assets/Mike.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mike : MonoBehaviour
+{
+    float horizontal;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Application.platform==RuntimePlatform.Android)
+        { horizontal = Input.acceleration.x;
+        }else
+        {
+            horizontal = Input.GetAxis("Horizontal");
+        }
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * 10f, rb.velocity.y);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag=="Platform")
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(transform.up * 20, ForceMode2D.Impulse);
+        }
+    }
+}
